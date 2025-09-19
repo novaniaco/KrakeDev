cb4bfbffee30c7cff71a167922ad256ce2b5acc1
novaniaco <alejandrocuadros322@gmail.com>
2025-09-19 17:47:29 -0500

    Función mostrar texto implementada

--- EjercicioGit/utilitarios/saludo.js
+++ EjercicioGit/utilitarios/saludo.js
@@ -6,6 +6,13 @@ saludar=function(){
 
     let edad=recuperarInt("txtEdad");
     let estatura=recuperarFloat("txtEstatura");
+    let mensajeBienvenida="Bienvenido "+nombre+" "+apellido;
+    mostrarTexto("lblResultado",mensajeBienvenida);
+}
+mostrarTexto=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerText= mensaje;
 }
 recuperarTexto=function(idComponente){
     let componente;
