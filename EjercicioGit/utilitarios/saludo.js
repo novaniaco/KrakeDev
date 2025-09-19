d549a130b1092ebb4fe989d1241777b19e111098
novaniaco <alejandrocuadros322@gmail.com>
2025-09-19 12:53:32 -0500

    Se implementaron las funciones de recuperar Int y recuperar Float

--- EjercicioGit/utilitarios/saludo.js
+++ EjercicioGit/utilitarios/saludo.js
@@ -3,6 +3,9 @@ saludar=function(){
     let nombre=recuperarTexto("txtNombre");
     //recuperar el valor de la caja de texto txtApellido
     let apellido=recuperarTexto("txtApellido");
+
+    let edad=recuperarInt("txtEdad");
+    let estatura=recuperarFloat("txtEstatura");
 }
 recuperarTexto=function(idComponente){
     let componente;
@@ -10,4 +13,14 @@ recuperarTexto=function(idComponente){
     componente=document.getElementById(idComponente);
     valorIngresado=componente.value;
     return valorIngresado;
+}
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero
+}
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFloat=parseFloat(valorCaja);
+    return valorFloat
 }
