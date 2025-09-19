eb0ffc8017b4c71af317b6ef097fe22fc52560cb
novaniaco <alejandrocuadros322@gmail.com>
2025-09-18 19:15:53 -0500

    el botón modifica la imagen

--- EjercicioGit/ejercicio.js
+++ EjercicioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/ganar.gif"
     console.log("ingrese a saludar");
+
 }
