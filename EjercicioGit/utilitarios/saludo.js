23e452aed1a7427304e2d97fbbe9a7f6e732694d
novaniaco <alejandrocuadros322@gmail.com>
2025-09-20 22:27:35 -0500

    Se separan las funciones al archivo utilitarios.js

--- EjercicioGit/utilitarios/saludo.js
+++ EjercicioGit/utilitarios/saludo.js
@@ -9,31 +9,5 @@ saludar=function(){
     let mensajeBienvenida="Bienvenido "+nombre+" "+apellido;
     mostrarTexto("lblResultado",mensajeBienvenida);
     mostrarImagen("imgSaludo","./imagenes/panda.gif");
-}
-mostrarImagen=function(idComponente,rutaImagen){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.src=rutaImagen;
-}
-mostrarTexto=function(idComponente,mensaje){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.innerText= mensaje;
-}
-recuperarTexto=function(idComponente){
-    let componente;
-    let valorIngresado;
-    componente=document.getElementById(idComponente);
-    valorIngresado=componente.value;
-    return valorIngresado;
-}
-recuperarInt=function(idComponente){
-    let valorCaja=recuperarTexto(idComponente);
-    let valorEntero=parseInt(valorCaja);
-    return valorEntero
-}
-recuperarFloat=function(idComponente){
-    let valorCaja=recuperarTexto(idComponente);
-    let valorFloat=parseFloat(valorCaja);
-    return valorFloat
+    mostrarTextoEnCaja("txtNombre"," ");
 }
