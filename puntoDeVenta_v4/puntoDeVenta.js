c7a6b6e05ba16cbe3cd405f4a11f39c6ca13ae26
novaniaco <alejandrocuadros322@gmail.com>
2025-09-20 23:29:48 -0500

    Valor Total exitoso

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -66,22 +66,23 @@ calcularValorTotal = function () {
             Si el caso de prueba es exitoso, hacer un commit
         */
     //10. Invocar a calcularTotal y lo que devuelve guardar en la variable valorTotal
+    valorTotal=calcularTotal(valorSubtotal,valorDescuento,valorIVA);
     //11. Mostrar el resultado en el componente lblTotal
+    mostrarTexto("lblTotal",valorTotal);
     /*
         Caso de prueba: 
             - cantidad: 10
             - precioProducto: 5.4 
             - descuento: 10
 
-                --valorSubtotal: 5.4
-                --descuento: 5.4
-                --IVA: 5.832
+                --valorSubtotal: 54
+                --descuento: 0.54
+                --IVA: 6.4152
 
-                Total esperado: 54.432
+                Total esperado: 59.8752
 
                 Si el caso de prueba es exitoso, hacer un commit
-       */
-            
+    */           
     //12. Mostrar un resumen en el componente lblResumen, si no existe debe agregarlo
     /*
         Ejemplo: 
