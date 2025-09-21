eb03b401f1fed5c5c64e5854d0d5cd8a344b3bbf
novaniaco <alejandrocuadros322@gmail.com>
2025-09-20 23:26:12 -0500

    Valor IVA exitoso

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -47,8 +47,10 @@ calcularValorTotal = function () {
         Si el caso de prueba es exitoso, hacer un commit
      */
     //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
+    valorIVA=calcularIVA(valorSubtotal,valorDescuento);
     // El IVA debe calcularse sobre el valor del subtotal menos el descuento
-    //9. Mostrar el resultado en el componente lblValorIVA    
+    //9. Mostrar el resultado en el componente lblValorIVA  
+    mostrarTexto("lblValorIVA",valorIVA);  
         /*
             Caso de prueba: 
                 - cantidad: 10 
@@ -56,11 +58,10 @@ calcularValorTotal = function () {
                 - descuento: 10
 
                     - valorSubtotal: 54
-                    - descuento:5.4
+                    - descuento:0.54
                     - valorSubtotal 
-                    - descuento: 48.6
 
-                IVA esperado: 5.832
+                IVA esperado: 6.4152
 
             Si el caso de prueba es exitoso, hacer un commit
         */
