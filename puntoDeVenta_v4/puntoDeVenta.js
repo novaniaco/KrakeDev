b7e282b8cc2dadafb222626091957bbe496b51b4
novaniaco <alejandrocuadros322@gmail.com>
2025-09-20 23:35:50 -0500

    Resumen en el componente exitoso

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -84,12 +84,14 @@ calcularValorTotal = function () {
                 Si el caso de prueba es exitoso, hacer un commit
     */           
     //12. Mostrar un resumen en el componente lblResumen, si no existe debe agregarlo
+    let resumen;
+    resumen=("Valor a pagar por "+cantidad+" "+nombreProducto+" con "+porcentajeDescuento+"% de descuento: USD "+valorTotal);
+    mostrarTexto("lblResumen",resumen);
     /*
         Ejemplo: 
             Valor a pagar por 20 cerveza corona con 10% de descuento: USD 48.75
         Si funciona, hacer un commit
     */
-
 }
 limpiar = function () {
     /*
