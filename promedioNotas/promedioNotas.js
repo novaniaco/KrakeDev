26fc3837c46257e0066fa7d1e073108c13bd99ac
novaniaco <alejandrocuadros322@gmail.com>
2025-09-22 23:08:06 -0500

    Se agregaron los gifts de acuerdo al promedio obtenido y la página funciona según los reglamentos emitidos

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -1,12 +1,16 @@
 calcularPromedioNotas=function(){
-    //Recuperar valores como Float y guardar en una variable
    let n1Float = recuperarFloat("txtn1");
    let n2Float = recuperarFloat("txtn2");
    let n3Float = recuperarFloat("txtn3");
-    //Invocar a la función calcularPromedio, pasándole como parametro las 3 notas guardar el retorno en una variable llamada promedio
     let promedio=calcularPromedio(n1Float,n2Float,n3Float);
     let resultadoFormateado=promedio.toFixed(2);
 
     mostrarTexto("lblPromedio",resultadoFormateado);
-
+    if(resultadoFormateado>7){
+        mostrarTexto("lblMensaje","APROBADO");
+        mostrarImagen("gift","./gifts/exito.gif");
+    }else{
+        mostrarTexto("lblMensaje","REPROBADO");
+        mostrarImagen("gift","./gifts/fracaso.gif");   
+    }
 }
