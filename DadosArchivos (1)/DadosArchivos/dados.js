b7cd1fbb2780818015fe74514d79eac517cc7a70
novaniaco <alejandrocuadros322@gmail.com>
2025-09-22 21:37:16 -0500

    Se implemento la función random y se referenció el archivo utilitarios.js (La página es funcional)

---
+++ DadosArchivos (1)/DadosArchivos/dados.js
@@ -0,0 +1,20 @@
+jugar=function(){
+    let aleatorio;
+    aleatorio=lanzarDado();
+    cambiarTexto("lblNumero",aleatorio);
+}
+
+//Crear una función llamada lanzarDado
+//No recibe parámetros
+//Retorna un número aleatorio entre 1 y 6
+lanzarDado=function(){
+    let aleatorio;
+    let numeroMultiplicado;
+    let numeroEntero;
+    let valorDado;
+    aleatorio=Math.random();  //entre 0 y 1
+    numeroMultiplicado=aleatorio*6;
+    numeroEntero=parseInt(numeroMultiplicado); //entre 0 y 5
+    valorDado=numeroEntero+1; //entre 1 y 6
+    return valorDado;
+}   
