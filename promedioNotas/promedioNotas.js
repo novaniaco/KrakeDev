8c3a091f07d43785fa9cbd367c961d90a7f424e7
novaniaco <alejandrocuadros322@gmail.com>
2025-09-26 16:32:29 -0500

    Modificación de Gifts en el archivo de Promedio Notas

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -6,11 +6,18 @@ calcularPromedioNotas=function(){
     let resultadoFormateado=promedio.toFixed(2);
 
     mostrarTexto("lblPromedio",resultadoFormateado);
-    if(resultadoFormateado>7){
-        mostrarTexto("lblMensaje","APROBADO");
-        mostrarImagen("gift","./gifts/exito.gif");
-    }else{
+
+    if(resultadoFormateado<5 && resultadoFormateado>0){
         mostrarTexto("lblMensaje","REPROBADO");
-        mostrarImagen("gift","./gifts/fracaso.gif");   
+        mostrarImagen("gift","./gifts/reprobado.gif");
+    }else if(resultadoFormateado>=5 && resultadoFormateado<=8){
+         mostrarTexto("lblMensaje","BUEN TRABAJO");
+        mostrarImagen("gift","./gifts/buenTrabajo.gif");
+    }else if(resultadoFormateado>8 && resultadoFormateado<=10){
+         mostrarTexto("lblMensaje","EXCELENTE");
+        mostrarImagen("gift","./gifts/excelente.gif");
+    }else{
+        mostrarTexto("lblMensaje","Los datos ingresados son Incorrectos!!");
+        mostrarImagen("gift","");   
     }
 }
