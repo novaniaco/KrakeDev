c10dd434a661d129d0e5e0a9857790f2a1de3549
novaniaco <alejandrocuadros322@gmail.com>
2025-10-04 08:37:45 -0500

    PASO 1: Implementada la función guardarPalabra con validación de 5 letras mayúsculas

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -9,3 +9,22 @@ esMayuscula=function(caracter){
     let mayuscula=caracter.charCodeAt(0);
     return mayuscula>=65 && mayuscula <=90;
 }
+//PASO 1 INGRESO DE LA PALABRA SECRETA 
+guardarPalabra=function(){
+    let palabra=recuperarTexto("txtSecreta");
+    
+    if (palabra.length !== 5) {
+        alert("Debe ingresar una palabra de 5 letras mayusculas.");
+        return;
+    }
+
+    for (let i = 0; i < palabra.length; i++) {
+        if (!esMayuscula(palabra[i])) {
+            alert("Debe ingresar SOLO letras mayusculas.");
+            return;
+        }
+    }
+    palabraSecreta = palabra;
+    console.log("Palabra secreta guardada:", palabraSecreta);
+    alert("Palabra guardada correctamente");
+}    
